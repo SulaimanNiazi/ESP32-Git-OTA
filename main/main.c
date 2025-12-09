c5fdee50cac76c961fb5aa76b4d5e3089f46229d
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-12-09 16:41:58 +0500

    Add function for checking and saving required values from nvs

--- main/main.c
+++ main/main.c
@@ -8,23 +8,40 @@
 #define tx_pin          1
 #define rx_pin          3
 
+char* nvs_uart_read(const char* key, const char* name){
+    char* prev = nvs_read(key);
+    if(prev[0]) return prev;
+    
+    char prompt[NVS_MAX_LENGTH];
+    while(1){
+        sprintf(prompt, "\r\n%s not found, Enter %s: ", name, name);
+        char* value = uart_read(uart_num, prompt);
+        if(value[0]){
+            bool success = nvs_write(key, value);
+            if(success){
+                uart_write(uart_num, "Saved successfully\r\n");
+                size_t size = 1;
+                for(; value[size]; size++);
+                char* output = malloc(size);
+                sprintf(output, "%s", value);
+                return output;
+            }else{
+                uart_write(uart_num, "Save failed\r\n");
+                return "";
+            }
+        }else uart_write(uart_num, "Invalid input\r\n");
+    }
+}
+
 void app_main(){
     set_pin(led_pin, GPIO_MODE_OUTPUT, false, false);
     init_uart(uart_num, 115200, tx_pin, rx_pin);
     init_nvs();
 
-    char *wifi_ssid = nvs_read("wifi_ssid");
-    char *wifi_pin = nvs_read("wifi_pin");
-    if(wifi_ssid[0]){
-        uart_write(uart_num, "SSID: "); uart_write(uart_num, wifi_ssid);
-        uart_write(uart_num, "\r\nPin: "); uart_write(uart_num, wifi_pin);
-    } else {
-        uart_write(uart_num, "WiFi SSID not found.\r\n");
-        char* ssid = uart_read(uart_num, "Enter WiFi SSID: ");
-        nvs_write("wifi_ssid", ssid);
-        char *pin = uart_read(uart_num, "Enter WiFi Pin: ");
-        nvs_write("wifi_pin", pin);
-    }
+    char* wifi_ssid = nvs_uart_read("wifi_ssid", "WiFi SSID"),* wifi_pin = nvs_uart_read("wifi_pin", "WiFi Pin");
+    uart_write(uart_num, wifi_ssid);
+    uart_write(uart_num, " ");
+    uart_write(uart_num, wifi_pin);
 
     while(1){
         gpio_set_level(led_pin, 1);
