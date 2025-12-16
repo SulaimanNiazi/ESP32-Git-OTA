1750a80155d535a261c14d81960a5878b2c5ac18
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-12-16 18:37:45 +0500

    Simplify http client configuration in ota module by removing unnecessary settings.

--- main/ota.c
+++ main/ota.c
@@ -7,24 +7,26 @@
 static char ota_buffer[OTA_BUFFER_SIZE], current_version[32], latest_version[32];
 static bool up_to_date = true;
 
+esp_http_client_config_t get_client_config(const char *url){
+    esp_http_client_config_t config = {
+        .url                = url,
+        .crt_bundle_attach  = esp_crt_bundle_attach,
+    };
+    return config;
+}
+
 void check_ota(){
     const esp_app_desc_t *app = esp_app_get_description();
     sprintf(current_version, "%s", app->version);
 
-    esp_http_client_config_t config = {
-        .method             = HTTP_METHOD_GET,
-        .url                = OTA_VERSION_URL,
-        .host               = OTA_HOST,
-        .port               = OTA_PORT,
-        .crt_bundle_attach  = esp_crt_bundle_attach,
-    };
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-    if(esp_http_client_open(client, 0) == ESP_OK){
-        if(esp_http_client_fetch_headers(client)){
-            int len = esp_http_client_read(client, ota_buffer, OTA_BUFFER_SIZE);
-            esp_http_client_cleanup(client);
+    esp_http_client_config_t config = get_client_config(OTA_VERSION_URL);
+    esp_http_client_handle_t handle = esp_http_client_init(&config);
+    if(esp_http_client_open(handle, 0) == ESP_OK){
+        if(esp_http_client_fetch_headers(handle)){
+            int len = esp_http_client_read(handle, ota_buffer, OTA_BUFFER_SIZE);
+            esp_http_client_cleanup(handle);
             
-            if(len){ // Should be >= 366
+            if(len){ // Should be c.366
                 for(; ota_buffer[--len] != ' ';); // Find the last ' '
                 for(size_t ver_ind = 0, buf_ind = len; ota_buffer[++buf_ind] != ')'; ver_ind++){
                     latest_version[ver_ind] = ota_buffer[buf_ind];
@@ -34,7 +36,7 @@ void check_ota(){
             }
         }
     }
-    esp_http_client_cleanup(client);
+    esp_http_client_cleanup(handle);
     sprintf(latest_version, "%s", current_version);
 }
 
@@ -49,11 +51,7 @@ bool ota_up_to_date(){
 void ota_update(){
     if(up_to_date || OTA_DISABLED) return;
     
-    esp_http_client_config_t client_config = {
-        .url                = OTA_BIN_URL,
-        .crt_bundle_attach  = esp_crt_bundle_attach,
-        .timeout_ms         = 15000,
-    };
+    esp_http_client_config_t client_config = get_client_config(OTA_BIN_URL);
     esp_https_ota_config_t ota_config = {.http_config = &client_config};
     if(esp_https_ota(&ota_config) == ESP_OK) esp_restart();
 }
--- main/ota.h
+++ main/ota.h
@@ -3,8 +3,6 @@
 #define OTA_DISABLED     false
 #define OTA_VERSION_URL "https://raw.githubusercontent.com/SulaimanNiazi/ESP32-Git-OTA/refs/heads/main/CMakeLists.txt"
 #define OTA_BIN_URL     "https://raw.githubusercontent.com/SulaimanNiazi/ESP32-Git-OTA/refs/heads/main/build/Git_OTA.bin"
-#define OTA_HOST        "raw.githubusercontent.com"
-#define OTA_PORT        443
 #define OTA_BUFFER_SIZE 500
 
 #include "esp_system.h"
