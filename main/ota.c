b47914df8521d95857a478131e3f06b6ab310109
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-12-17 17:53:03 +0500

    Make OTA module read the latest version from the bin file, to reduce complexity and dependance on CMakeLists file

--- main/ota.c
+++ main/ota.c
@@ -6,39 +6,32 @@
 
 static char ota_buffer[OTA_BUFFER_SIZE], current_version[32], latest_version[32];
 static bool up_to_date = true;
-
-esp_http_client_config_t get_client_config(const char *url){
-    esp_http_client_config_t config = {
-        .url                = url,
-        .crt_bundle_attach  = esp_crt_bundle_attach,
-    };
-    return config;
-}
+static const esp_http_client_config_t http_client = {
+    .url                = OTA_BIN_URL,
+    .crt_bundle_attach  = esp_crt_bundle_attach,
+};
 
 void check_ota(){
     const esp_app_desc_t *app = esp_app_get_description();
     sprintf(current_version, "%s", app->version);
+    esp_http_client_handle_t handle = esp_http_client_init(&http_client);
 
-    esp_http_client_config_t config = get_client_config(OTA_VERSION_URL);
-    esp_http_client_handle_t handle = esp_http_client_init(&config);
+    bool failed = true;
     if(esp_http_client_open(handle, 0) == ESP_OK){
         if(esp_http_client_fetch_headers(handle)){
             int len = esp_http_client_read(handle, ota_buffer, OTA_BUFFER_SIZE);
-            esp_http_client_cleanup(handle);
-            
-            if(len){ // Should be c.366
-                while(ota_buffer[--len] != ' ');
-                while(ota_buffer[--len] != ' ');
-                for(size_t ver_ind = 0, buf_ind = len; ota_buffer[++buf_ind] != ')'; ver_ind++){
-                    latest_version[ver_ind] = ota_buffer[buf_ind];
-                    if(latest_version[ver_ind] != current_version[ver_ind]) up_to_date = false;
-                }
-                return;
-            }
+            failed = len < OTA_VERSION_INDEX;
         }
     }
     esp_http_client_cleanup(handle);
-    sprintf(latest_version, "%s", current_version);
+    
+    if(failed) sprintf(latest_version, "%s", current_version);
+    else{
+        for(size_t vi = 0, bi = OTA_VERSION_INDEX; ota_buffer[++bi]; vi++){
+            latest_version[vi] = ota_buffer[bi];
+            if(latest_version[vi] != current_version[vi]) up_to_date = false;
+        }
+    }
 }
 
 char *ota_get_version(const bool latest){
@@ -52,7 +45,6 @@ bool ota_up_to_date(){
 void ota_update(){
     if(up_to_date || OTA_DISABLED) return;
     
-    esp_http_client_config_t client_config = get_client_config(OTA_BIN_URL);
-    esp_https_ota_config_t ota_config = {.http_config = &client_config};
+    esp_https_ota_config_t ota_config = {.http_config = &http_client};
     if(esp_https_ota(&ota_config) == ESP_OK) esp_restart();
 }
