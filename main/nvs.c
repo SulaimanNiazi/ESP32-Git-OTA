9bc2eeaa215c1922b6ca71a2eb4d989f611e1f32
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-12-23 12:07:35 +0500

    Add logging to NVS module

--- main/nvs.c
+++ main/nvs.c
@@ -1,6 +1,8 @@
 #include "nvs.h"
-#include "nvs_flash.h"
 #include "gpio.h"
+#include "log.h"
+
+#include <nvs_flash.h>
 
 static void boot_handler(void *arg){
     while(1){
@@ -9,74 +11,43 @@ static void boot_handler(void *arg){
     }
 }
 
-void error_handler(const esp_err_t error){
-    switch(error){
-        case ESP_OK: break;
-        case ESP_ERR_NO_MEM: break;
-        case ESP_ERR_NVS_NOT_ENOUGH_SPACE: break;
-        case ESP_ERR_NVS_PAGE_FULL: break;
-        case ESP_ERR_NVS_NO_FREE_PAGES: break;
-        case ESP_ERR_NVS_NEW_VERSION_FOUND: break;
-
-        case ESP_ERR_NVS_INVALID_LENGTH: break;
-        case ESP_ERR_NVS_INVALID_HANDLE: break;
-        case ESP_ERR_NVS_INVALID_STATE: break;
-        case ESP_ERR_NVS_TYPE_MISMATCH: break;
-
-        case ESP_ERR_NVS_KEY_TOO_LONG: break;
-        case ESP_ERR_NVS_VALUE_TOO_LONG: break;
-
-        case ESP_ERR_NVS_NOT_FOUND:
-        case ESP_ERR_NVS_NOT_INITIALIZED:
-        case ESP_ERR_NVS_KEYS_NOT_INITIALIZED: break;
-        case ESP_ERR_NVS_PART_NOT_FOUND:
-        case ESP_ERR_NVS_READ_ONLY:
-        case ESP_ERR_NVS_CORRUPT_KEY_PART: break;
-        case ESP_ERR_NVS_REMOVE_FAILED:
-
-        case ESP_ERR_NVS_WRONG_ENCRYPTION:
-        case ESP_ERR_NVS_XTS_CFG_FAILED:
-        case ESP_ERR_NVS_XTS_DECR_FAILED:
-        case ESP_ERR_NVS_XTS_CFG_NOT_FOUND:
-        case ESP_ERR_NVS_XTS_ENCR_FAILED:
-        default: break;
-    }
-}
-
-bool init_nvs(){
-    esp_err_t err = nvs_flash_init();
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+void init_nvs(){
+    esp_err_t error = nvs_flash_init();
+    if (error == ESP_ERR_NVS_NO_FREE_PAGES || error == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         nvs_flash_erase();
-        err = nvs_flash_init();
-    }
+        error = nvs_flash_init();
+    } else log_error(NVS_LOG_TAG, error, "Failed to initialize NVS");
 
-    bool set = set_pin(BOOT_PIN, GPIO_MODE_INPUT, true, false);
-    if(set) xTaskCreate(boot_handler, "boot_handler", 2048, NULL, 10, NULL);
-    else err--;
-    return err == ESP_OK;
+    if(set_pin(BOOT_PIN, GPIO_MODE_INPUT, true, false)) xTaskCreate(boot_handler, "boot_handler", 2048, NULL, 10, NULL);
+    else log_error(NVS_LOG_TAG, ESP_FAIL, "Failed to create boot pin handler.");
 }
 
 bool nvs_write(const char *key, const char *value){
+    bool success = false;
     nvs_handle_t handle;
-    esp_err_t err = nvs_open(NAMESPACE, NVS_READWRITE, &handle);
-    err += nvs_set_str(handle, key, value);
-    err += nvs_commit(handle);
-    nvs_close(handle);
-    return err == ESP_OK;
+    if(!log_error(NVS_LOG_TAG, nvs_open(NAMESPACE, NVS_READWRITE, &handle), "Failed to open NVS in rread-write mode.")){
+        if(!log_error(NVS_LOG_TAG, nvs_set_str(handle, key, value), "Failed to set string for given key."))
+        if(!log_error(NVS_LOG_TAG, nvs_commit(handle), "NVS commit failed")) success = true;
+        nvs_close(handle);
+    }
+    return success;
 }
 
 char *nvs_read(const char *key){
+    char *buffer = "";
     nvs_handle_t handle;
-    esp_err_t err = nvs_open(NAMESPACE, NVS_READONLY, &handle);
-    size_t size = 0;
-    err += nvs_get_str(handle, key, NULL, &size);
-    if(err == ESP_OK && size != 0){
-        char *buffer = malloc(size);
-        err = nvs_get_str(handle, key, buffer, &size);
+    if(!log_error(NVS_LOG_TAG, nvs_open(NAMESPACE, NVS_READONLY, &handle), "Failed to open NVS in read mode")){
+        size_t size = 0;
+        if(!log_error(NVS_LOG_TAG, nvs_get_str(handle, key, NULL, &size), "Failed to get string size")){
+            if(size){
+                buffer = malloc(size);
+                if(log_error(NVS_LOG_TAG, nvs_get_str(handle, key, buffer, &size), "Failed to get string value for given key")){
+                    free(buffer);
+                    buffer = NULL;
+                }
+            }
+        }
         nvs_close(handle);
-        return buffer;
-    } else {
-        nvs_close(handle);
-        return "";
     }
+    return buffer;
 }
--- main/nvs.h
+++ main/nvs.h
@@ -2,10 +2,11 @@
 
 #include "delay.h"
 
-#define NAMESPACE      "OTA"
-#define BOOT_PIN       0
-#define NVS_MAX_LENGTH 1024
+#define NAMESPACE       "OTA"
+#define NVS_LOG_TAG     "NVS"
+#define BOOT_PIN        0
+#define NVS_MAX_LENGTH  1024
 
-bool init_nvs();
+void init_nvs();
 bool nvs_write(const char *key, const char *value);
 char *nvs_read(const char *key);
